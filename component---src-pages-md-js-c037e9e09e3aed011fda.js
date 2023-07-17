236410a866cb505d6a3ab6e9edd3bb523aecae60
Matt Franchi <mattwfranc@gmail.com>
2023-07-17 13:42:31 -0400

    Updates

---
+++ component---src-pages-md-js-c037e9e09e3aed011fda.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkd2pd3=self.webpackChunkd2pd3||[]).push([[888],{2206:function(e,t,n){n.r(t),n.d(t,{default:function(){return l}});var r=n(7294);function l(e){let{data:t}=e;const n=t.markdownRemark;return r.createElement("div",null,r.createElement("h1",null,n.frontmatter.title),r.createElement("div",{dangerouslySetInnerHTML:{__html:n.html}}))}}}]);
+//# sourceMappingURL=component---src-pages-md-js-c037e9e09e3aed011fda.js.map
